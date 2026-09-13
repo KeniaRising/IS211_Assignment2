df8c45a31e623819126c858758113f25927a93b5
Kenia Ventura <kv24289741@SPS-IS211126910.sps.cuny.edu>
2026-09-13 19:33:39 -0400

    submit

--- assignment2.py
+++ assignment2.py
@@ -1,37 +1,98 @@
 import argparse
-import urllib.request
-import logging
+import csv
 import datetime
+import logging
+from py_compile import main
+import urllib.request 
+
 
 def download_data(url):
-    """
-    Reads data from a URL and returns the data as a string
+    response = urllib.request.urlopen(url)
+    data = response.read()
+    return data 
 
-    :param url:
-    :return: the content of the URL
-    """
-    # read the URL
-    with urllib.request.urlopen(url) as response:
-        response = response.read().decode('utf-8')
+def processData (csvData):
+    personData = {}
 
-    # return the data
-    return response
+    logger = logging.getLogger('assignment2')
 
+    lines = csvData.decode('utf-8').splitlines()
+    reader = csv.reader(lines)
 
-def processData(file_content):
-    pass
+    for lineNum, row in enumerate(reader, starter=1):
+
+        if lineNum <= 2:
+            continue
+
+        personID = int(row[0])
+        name = row[1]
+        birthday = row[2]
+
+    try:
+        birthday = datetime.datetime.strptime(
+            birthday, "%d/%m/%Y"
+        )
+        personData[personID] = (name, birthday)
+    except ValueError:
+        logger.error(
+            "Error processing line #%d for ID #%d",
+            lineNum, 
+            personID
+        )
+    return personData
 
 
 def displayPerson(id, personData):
-    pass
+    if id not in personData:
+        print("No user found with that id")
+    else:
+        name, birthday = personData[id]
 
-def main(url):
-    print(f"Running main with URL = {url}...")
+        print(
+            "Person #%d is %s with a birhtday of %s"
+            % (id, name, birthday.strftime("%Y-%m-%d"))
+        )
+
+
+    def main():
+        parser = argparse.ArgumentParser()
+
+        parser.add_argument(
+            '--url',    
+            required=True,
+            help='URL to the CSV file'
+        )
+
+        args = parser.parse_args()
+
+        try: 
+            csvData = download_data(args.url)
+        except Exception as e:
+            print("Error downloading data:", e)
+            return
+        logging.basicConfig(
+            filename= 'error.log',
+            level=logging.ERROR,
+            format='%(message)s'
+        )
+
+        personData = processData(csvData)
+
+        while True:
+            try:
+                id = int(input("Enter an ID number: "))
+            except ValueError:
+                print("Please enter a number.")
+                continue
+
+            if id <= 0:
+                break
+
+            displayPerson(id, personData)
+
 
 
 if __name__ == "__main__":
-    """Main entry point"""
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--url", help="URL to the datafile", type=str, required=True)
-    args = parser.parse_args()
-    main(args.url)
+    main()
+
+   
